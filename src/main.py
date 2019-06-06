b91babe52a61cad189a6b8d052a36e53babead49
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-06 20:07:53 +0800

    git pydantic and orders matter

--- src/main.py
+++ src/main.py
@@ -4,9 +4,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
 
 
 if __name__ == "__main__":
