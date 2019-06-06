fc0e37942b28c336af63ba8a736a4f71cba48943
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-07 07:25:00 +0800

    2.4.3 Required query parameters extra parameters with require or not

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -8,12 +9,9 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_user_item(
-        item_id: str, needy: str, skip: int=0, limit: int=None
+        item_id: str, limit: Optional[int] = None
 ):
-    item = {
-        "item_id": item_id, "needy": needy,
-        "skip": skip, "limit": limit
-    }
+    item = {"item_id": item_id, "limit": limit}
     return item
 
 if __name__ == "__main__":
