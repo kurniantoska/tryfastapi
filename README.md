d37e8d1257f9731db2fa9c1a7301bdd096915628
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-06 19:37:20 +0800

    first_guide

---
+++ README.md
@@ -0,0 +1,2 @@
+# tryfastapi
+Try FastAPI
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app, host="127.0.0.1", port=8000,
+        reload=True, log_level="debug",
+    )
