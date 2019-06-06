3b2606f3532df412927bae4b971d6535680fcb81
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-07 06:55:18 +0800

    2.3 Multipath and query parameters

--- src/main.py
+++ src/main.py
@@ -6,17 +6,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str=None, short: bool=False):
-    item = {"item_id": item_id}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: str=None, short: bool=False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
     if q:
         item.update({"q": q})
     if not short:
         item.update(
-            {
-                "decription": "This is an amazing item "
-                              "that has a long description "
-            }
+            {"description": "This is an amazing item that has a long description"}
         )
     return item
 
