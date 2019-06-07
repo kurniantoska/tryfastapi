9a199f823311e961f5faff9e73a1fbf7910a7882
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-07 08:41:23 +0800

    3.4. request body path plus parameter plus query

--- src/main.py
+++ src/main.py
@@ -15,8 +15,11 @@ app = FastAPI()
 
 
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
+async def create_item(item_id: int, item: Item, q: str = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 if __name__ == "__main__":
