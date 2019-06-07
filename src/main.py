8f4a6b4f1b83a7eee43073c6bccf9ec1b00de430
Riska K Abdullah <alternative.xen.sakura@gmail.com>
2019-06-07 08:38:10 +0800

    3.3 request body plus path

--- src/main.py
+++ src/main.py
@@ -14,13 +14,9 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
 
 
 if __name__ == "__main__":
